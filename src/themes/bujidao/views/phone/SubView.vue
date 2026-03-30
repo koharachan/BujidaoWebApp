d824b4f889f5eec77ec9936fb32a047e2ef8cf3c
0 <0>
2026-03-30 17:25:49 +0800

    手机端增加纯文本

--- src/themes/bujidao/views/phone/SubView.vue
+++ src/themes/bujidao/views/phone/SubView.vue
@@ -55,29 +55,18 @@ function change_wireguard_pri_key() {
         <h2>操作</h2>
         <div class="box">
           <p>
-            <el-tooltip
-              content="订阅地址会被更换，重置后，所有使用订阅地址的地方都要进行替换"
-              placement="right"
-            >
+            <el-tooltip content="订阅地址会被更换，重置后，所有使用订阅地址的地方都要进行替换" placement="right">
               <el-button type="primary" @click="change_sub_key()" round>更换订阅地址</el-button>
             </el-tooltip>
           </p>
           <p>
-            <el-tooltip
-              content="订阅地址不会更换，重置后，所有使用订阅地址的客户端都需要更新订阅才能使用"
-              placement="right"
-            >
+            <el-tooltip content="订阅地址不会更换，重置后，所有使用订阅地址的客户端都需要更新订阅才能使用" placement="right">
               <el-button type="primary" @click="change_uuid()" round>重置代理密码</el-button>
             </el-tooltip>
           </p>
           <p>
-            <el-tooltip
-              content="Wireguard专属密钥，重置后，需要更改配置文件的私钥，或者重新复制配置文件"
-              placement="right"
-            >
-              <el-button type="primary" @click="change_wireguard_pri_key()" round
-                >重置Wireguard密码</el-button
-              >
+            <el-tooltip content="Wireguard专属密钥，重置后，需要更改配置文件的私钥，或者重新复制配置文件" placement="right">
+              <el-button type="primary" @click="change_wireguard_pri_key()" round>重置Wireguard密码</el-button>
             </el-tooltip>
           </p>
           <!-- <p v-for="(item, index ) in user_info.result?.sub_address" :key="item.index" @click="touchCopy(item)"
@@ -97,58 +86,42 @@ function change_wireguard_pri_key() {
         <h2>一键导入</h2>
         <div class="box">
           <p>
-            <a
-              :href="
-                encodeURI(
-                  'shadowrocket://add/sub://' +
-                    btoa(user_info.sub_address[0] + '&type=shadowrocket') +
-                    '?remark=布吉岛',
-                )
-              "
-              target="_blank"
-            >
+            <a :href="encodeURI(
+              'shadowrocket://add/sub://' +
+              btoa(user_info.sub_address[0] + '&type=shadowrocket') +
+              '?remark=布吉岛',
+            )
+              " target="_blank">
               <el-button type="primary" round> 导入Shadowrocket </el-button>
             </a>
           </p>
           <p>
-            <a
-              :href="
-                encodeURI(
-                  'sing-box://import-remote-profile?url=' +
-                    encodeURI(user_info.sub_address[0] + '&type=singbox') +
-                    '#布吉岛',
-                )
-              "
-              target="_blank"
-            >
+            <a :href="encodeURI(
+              'sing-box://import-remote-profile?url=' +
+              encodeURI(user_info.sub_address[0] + '&type=singbox') +
+              '#布吉岛',
+            )
+              " target="_blank">
               <el-button type="primary" round> 导入sing-box </el-button>
             </a>
           </p>
           <p>
-            <a
-              :href="
-                encodeURI(
-                  'sing-box://import-remote-profile?url=' +
-                    encodeURI(user_info.sub_address[0] + '&type=hidiffy') +
-                    '#布吉岛',
-                )
-              "
-              target="_blank"
-            >
+            <a :href="encodeURI(
+              'sing-box://import-remote-profile?url=' +
+              encodeURI(user_info.sub_address[0] + '&type=hidiffy') +
+              '#布吉岛',
+            )
+              " target="_blank">
               <el-button type="primary" round> 导入hidiffy </el-button>
             </a>
           </p>
           <p>
-            <a
-              :href="
-                encodeURI(
-                  'clash://install-config?url=' +
-                    encodeURI(user_info.sub_address[0] as string) +
-                    '&name=布吉岛',
-                )
-              "
-              target="_blank"
-            >
+            <a :href="encodeURI(
+              'clash://install-config?url=' +
+              encodeURI(user_info.sub_address[0] as string) +
+              '&name=布吉岛',
+            )
+              " target="_blank">
               <el-button type="primary" round> 一键导入Clash </el-button>
             </a>
           </p>
@@ -162,6 +135,13 @@ function change_wireguard_pri_key() {
           </p>
           <p @click="touchCopy(user_info.sub_address[1] as string)" class="hand">
             <el-button type="primary" round> 备用订阅 </el-button>
+          </p>
+          <!-- 订阅链接纯文本 -->
+          <p>
+            订阅链接：
+          </p>
+          <p class="no-wrap">
+            {{ user_info.sub_address[0] }}
           </p>
         </div>
       </div>
@@ -186,6 +166,7 @@ function change_wireguard_pri_key() {
   border: 1px solid var(--el-border-color);
   border-radius: 5px;
   overflow: hidden;
+  overflow-x: auto;
   // width: 100%;
   display: flex;
   flex-direction: column;
