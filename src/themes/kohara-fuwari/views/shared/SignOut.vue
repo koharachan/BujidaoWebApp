fc599d6a086fefbbe7de6abdaa37736300e953cd
0 <0>
2026-03-31 14:02:46 +0800

    登出后关闭窗口

--- src/themes/kohara-fuwari/views/shared/SignOut.vue
+++ src/themes/kohara-fuwari/views/shared/SignOut.vue
@@ -2,11 +2,15 @@
 import { ElMessage } from 'element-plus'
 import { useUserInfoStore } from '@/stores/userInfoStore'
 import { useRouter } from 'vue-router'
+import { closeTelegramWebApp } from '@/utils/telegram'
 const router = useRouter()
 
 const userInfoStore = useUserInfoStore()
 userInfoStore.clearUserInfo()
 ElMessage.success('已退出登录')
+setTimeout(() => {
+closeTelegramWebApp()
+}, 233)
 setTimeout(() => {
   router.push({
     path: '/login',
